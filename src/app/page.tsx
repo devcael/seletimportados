3e2294f8c5d72effa45b30c37845cd699f13230e
root <root@DESKTOP-A0NIB06>
2023-08-01 07:08:34 -0300

    create login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 "use client"
 import { InputHookForm } from '@/components/InputHookForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './globals.css'
 import "@styles/fonts.css"
 import "@styles/teste.css"
 import { Inter } from "next/font/google";
 import styled from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
+
+
+import { Button, Divider, Form, Grid, Segment } from 'semantic-ui-react';
 const inter = Inter({ subsets: ['latin'] })
 
 const Body = styled.div`
@@ -32,40 +38,46 @@ const TitleH5 = styled.h5`
 `
 
 const CustomImg = styled.img`
-  width: 100vw;
+  width: 40vw;
   height: 100vh;
-  position: absolute;
+  border-radius: 0px 16px 16px 0px;
+  position: relative;
 `
 
-const GlassDiv = styled.div`
-  width: 100vw;
-  height: 100vh;
-  position: absolute;
-  background: rgba(255, 255, 255, 0.13);
-  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-  backdrop-filter: blur(5px);
-  -webkit-backdrop-filter: blur(5px);
-  border: 1px solid rgba(255, 255, 255, 0.3);
-`
 
 const LoginContainer = styled.div`
   display: flex;
-  padding: 20px;
+  padding: 0px 280px;
   align-items: center;
+  justify-content: center;
   flex-direction: column;
   position: relative;
-  width: 100vw;
+  width: 60vw;
   height: 100vh;
-  max-width: 600px;
-  max-height: 800px;
-  background-color: red;
   border-radius: 10px;
   background: rgba(255, 255, 255, 0.85);
-  border-radius: 16px;
   box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
   backdrop-filter: blur(5px);
   -webkit-backdrop-filter: blur(5px);
   border: 1px solid rgba(255, 255, 255, 0.3);
+`
+
+const IconWrapper = styled.div`
+  position: relative;
+  padding: 10px 50px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const LoginButton = styled.button`
+  width: 100%;
+  max-width: 500px;
+  background: var(--blue-ascent);
+  padding: 10px 0px;
+  border: none;
+  border-radius: 5px;
 `
 
 function InputWithLabel() {
@@ -77,22 +89,37 @@ function InputWithLabel() {
 export default function Home() {
 
   return (
-    <div style={{ width: "100vw", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center", backgroundColor: "red" }} >
-      <CustomImg width={100} className="fullwidth-img" src="wallpaper-fundo-iphone.jpg" alt="" />
-      <GlassDiv></GlassDiv>
+    <div style={{ width: "100vw", height: "100vh", display: "flex", alignItems: "start", justifyContent: "start" }} >
+      <CustomImg width={100} className="fullwidth-img" src="login_background.png" alt="" />
       <LoginContainer>
         <div style={{ width: 100, borderRadius: 5, height: "100px", overflow: "hidden", position: "relative" }}>
           <img style={{ height: "100%", position: "relative" }} className="fullwidth-img" src="logo.png" alt="" />
         </div>
-        <div>
-          <h1>SELET IMPORTADOS</h1>
+        <div style={{ textAlign: "center", margin: "20px 0px" }}>
+          <h1 style={{ color: "teal" }}>SELET</h1>
+          <h3>IMPORTADOS</h3>
         </div>
-
-        <div>
+        <IconWrapper>
+          <div style={{ flexGrow: "1", maxWidth: "150px", height: "3px", borderRadius: "50px", backgroundColor: "grey" }}></div>
+          <i style={{ margin: "0px 15px" }} className="fa-solid fa-globe"></i>
+          <div style={{ flexGrow: "1", maxWidth: "150px", height: "3px", borderRadius: "50px", backgroundColor: "grey" }}></div>
+        </IconWrapper>
+        <div style={{ textAlign: "center", margin: "10px 0px" }}>
           <h3>BEM VINDOS</h3>
-          <h5>GERENCIADOR DE VENDAS</h5>
+          <p>GERENCIADOR DE VENDAS</p>
         </div>
+        <div className="input-group mb-3" style={{ height: "60px", maxWidth: "500px" }}>
+          <span className="input-group-text" id="basic-addon1" style={{ padding: "10px 20px", display: "flex", alignItems: "center", justifyContent: "center" }}><i className="fa-solid fa-user"></i></span>
+          <input type="text" className="form-control" placeholder="Digite seu usuario" aria-label="Username" aria-hidden="true" aria-describedby="basic-addon1" />
+        </div>
+        <div className="input-group mb-3" style={{ height: "60px", maxWidth: "500px" }}>
+          <span className="input-group-text" id="basic-addon1" style={{ padding: "10px 20px", display: "flex", alignItems: "center", justifyContent: "center" }}><i className="fa-solid fa-lock"></i></span>
+          <input type="password" className="form-control" placeholder="Digite sua senha" aria-label="Username" aria-hidden="true" aria-describedby="basic-addon1" />
+        </div>
+        <LoginButton>LOGIN</LoginButton>
       </LoginContainer>
     </div>
   )
 }
+
+
