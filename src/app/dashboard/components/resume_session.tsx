deaf62e5d03b4b4c6e02ea38a2c476168575cef6
DevCael <caelgoncalves@icloud.com>
2023-08-30 21:47:09 -0300

    .

--- src/app/dashboard/components/resume_session.tsx
+++ src/app/dashboard/components/resume_session.tsx
@@ -21,10 +21,10 @@ function ResumeSessionContainer() {
     const { vendaData } = useVendaContext();
 
     return (<ResumeSession>
-        <CardResume icon="fa-solid fa-chart-simple" color="var(--secodary-blue)" label='Receita Total' value={StrUtil.formatadorComSufixoComGarantiaDeDecimal(vendaData.dadosEstatisticos?.total_receita.toString() ?? "0.00")}></CardResume>
-        <CardResume icon="fa-solid fa-chart-line" color="teal" label='Lucro' value={StrUtil.formatadorComSufixoComGarantiaDeDecimal(vendaData.dadosEstatisticos?.lucro.toString() ?? "0.00")}></CardResume>
-        <CardResume icon="fa-regular fa-chart-bar" color="var(--color-primary)" label='Qnt de Vendas' value={vendaData.dadosEstatisticos?.quantidade_vendas.toString() ?? "0"}></CardResume>
-        <CardResume icon="fa-solid fa-boxes-stacked" color="var(--blue-ascent)" label='Produto Mais Vendido' value={vendaData.dadosEstatisticos?.produto_mais_vendido.toString() ?? "Não foi possivel identificar"}></CardResume>
+        <CardResume icon="fa-solid fa-chart-simple" color="var(--secodary-blue)" label='Receita Total' value={StrUtil.formatadorComSufixoComGarantiaDeDecimal(vendaData.dadosEstatisticos?.total_receita?.toString() ?? "0.00")}></CardResume>
+        <CardResume icon="fa-solid fa-chart-line" color="teal" label='Lucro' value={StrUtil.formatadorComSufixoComGarantiaDeDecimal(vendaData.dadosEstatisticos?.lucro?.toString() ?? "0.00")}></CardResume>
+        <CardResume icon="fa-regular fa-chart-bar" color="var(--color-primary)" label='Qnt de Vendas' value={vendaData.dadosEstatisticos?.quantidade_vendas?.toString() ?? "0"}></CardResume>
+        <CardResume icon="fa-solid fa-boxes-stacked" color="var(--blue-ascent)" label='Produto Mais Vendido' value={vendaData.dadosEstatisticos?.produto_mais_vendido?.toString() ?? "Não foi possivel identificar"}></CardResume>
     </ResumeSession>)
 }
 
